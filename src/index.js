932c40a04a1a3217db7a917927f7660aea7e3eb7
Ahmad Chata <ahmadchata@gmail.com>
2021-03-17 22:11:27 +0100

    Finish styling the contact page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import '../stylesheets/style.css';
 import loadInitial from './initialLoad';
+import contacts from './contact';
 import background from '../assets/background.jpeg';
 
 
@@ -9,7 +10,6 @@ const slogan = "Eat and rest or don't eat and continue ranting";
 const content = document.querySelector('#content');
 const body = document.querySelector('body');
 
-
-content.appendChild(loadInitial(title, slogan));
+content.appendChild(contacts());
 
 

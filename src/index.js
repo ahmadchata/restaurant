710533a3efeecfc1df2fad976567bcdb64c5e338
Ahmad Chata <ahmadchata@gmail.com>
2021-03-17 17:44:50 +0100

    Add styles file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import '../stylesheets/style.css';
 
+const content = document.querySelector('#content');
+const create = () => {
+  const p = document.createElement('p');
+  p.textContent = 'Restaurant';
+  p.classList.add('test');
+  return p;
+};
+
+content.appendChild(create());

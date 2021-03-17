bd2af06a7dfbdbd966e47a4ef29b473ea435f9bc
Ahmad Chata <ahmadchata@gmail.com>
2021-03-17 21:53:17 +0100

    Add background to initial page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import '../stylesheets/style.css';
 import loadInitial from './initialLoad';
+import background from '../assets/background.jpeg';
 
 
 const title = "Rest or Rant";
-const slogan = "You either eat and rest or don't eat and continue ranting";
+const slogan = "Eat and rest or don't eat and continue ranting";
 
 const content = document.querySelector('#content');
+const body = document.querySelector('body');
+
 
 content.appendChild(loadInitial(title, slogan));
+
+

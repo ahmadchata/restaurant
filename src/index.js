25556ad44243ae58863c8ed0e889513bb92ad272
Ahmad Chata <ahmadchata@gmail.com>
2021-03-17 23:31:22 +0100

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -11,6 +11,44 @@ const slogan = "Eat and rest or don't eat and continue ranting";
 const content = document.querySelector('#content');
 const body = document.querySelector('body');
 
-content.appendChild(menu());
+const navBar = document.createElement('nav');
+
+const navButton = (text) => {
+  const btn = document.createElement('button');
+  btn.textContent = text;
+  return btn;
+};
+
+navBar.appendChild(navButton('Home'));
+navBar.appendChild(navButton('Contact'));
+navBar.appendChild(navButton('Menu'));
+
+content.appendChild(navBar);
+
+const switchPage = () => {
+ document.querySelectorAll('button').forEach((e) => {
+  e.target.textContent;
+
+
+
+ })
+};
+
+document.querySelectorAll('button').forEach((b) => {
+  b.addEventListener('click', (e) => {
+     switch (e.target.textContent) {
+    case 'Home':
+      content.appendChild(loadInitial(title, slogan));
+      break;
+    case 'Contact':
+      content.appendChild(contacts());
+      break;
+    case 'Menu':
+      content.appendChild(menu());
+      break;
+    default:
+  }
+  })
+});
 
 
